b50f200e9244816830e568a20957a139f279e8d5
David Docampo <david@docampo.ch>
2024-01-31 22:57:40 +0100

    add logging and logic to admin creation

--- app/Console/Commands/CreateAdminUsers.php
+++ app/Console/Commands/CreateAdminUsers.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
@@ -27,11 +28,46 @@ class CreateAdminUsers extends Command
      */
     public function handle(): void
     {
-        User::factory()->create([
-            'name' => 'David Docampo',
-            'email' => 'david@docampo.ch',
-            'password' => Hash::make(openssl_random_pseudo_bytes(64)),
-            'admin' => true
-        ]);
+        $this->info('Fetching Administrator team...');
+        $team = Team::where('name', 'Administrators')->first();
+
+        $this->info('Fetching Administrator users...');
+        $davidDocampo = User::where([
+            ['email', 'david@docampo.ch'],
+            ['admin', true],
+        ])->first();
+
+        $this->info('Checking if Administrator users exist...');
+        if ($davidDocampo == null) {
+            $name = 'David Docampo';
+            $email = 'david@docampo.ch';
+
+            $this->info(sprintf('Creating user for %s (%s)...', $name, $email));
+            $davidDocampo = User::factory()->create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make(openssl_random_pseudo_bytes(64)),
+                'admin' => true
+            ]);
+        }
+
+        $this->info('Checking if Administrator team exists...');
+        if ($team == null) {
+            $name = 'Administrators';
+
+            $this->info(sprintf('Creating %s team...', $name));
+            $team = Team::factory()->create([
+                'name' => $name,
+                'user_id' => $davidDocampo->id,
+                'personal_team' => false
+            ]);
+        }
+
+        $this->info('Adding users to team...');
+        $team->users()->attach($davidDocampo->id, ['role' => 'admin']);
+
+        $davidDocampo->switchTeam($team);
+
+        $this->info('New users can reset their password via the login form');
     }
 }
