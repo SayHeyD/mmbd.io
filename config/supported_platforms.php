68aead34e50ae0a343528bcfababbed740888102
David Docampo <david@docampo.ch>
2024-02-03 19:55:43 +0100

    add config for supported platform URLs

---
+++ config/supported_platforms.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /**
+     * The supported Social media platform base URLs by the application
+     */
+    'base_urls' => [
+        'https://tiktok.com',
+        'https://youtube.com'
+    ]
+];
