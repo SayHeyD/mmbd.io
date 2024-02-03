518d040fea940d5fafb45fb1f958d9e955915f6e
David Docampo <david@docampo.ch>
2024-02-04 00:57:23 +0100

    add public post functions

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +18,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index'])->name('index');
 
+Route::prefix('/post')->name('post.')->group(function () {
+    Route::put('/', [PostController::class, 'store'])->name('store');
+    Route::get('/success', [PostController::class, 'success'])->name('success');
+    Route::get('/{post:slug}', [PostController::class, 'show'])->name('show');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::prefix('post')->name('post.')->group(function () {
+    });
 });
