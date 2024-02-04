9eb41098cba7e57e489490953eb49ab49fe1141e
David Docampo <david@docampo.ch>
2024-02-04 18:39:53 +0100

    add type field and type detection methods

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Nette\NotImplementedException;
 
 class Post extends Model
 {
@@ -15,6 +16,61 @@ class Post extends Model
      * @var array<int, string>
      */
     protected $fillable = [
-        'team_id', 'name', 'slug', 'original_url',
+        'team_id', 'name', 'type', 'slug', 'original_url',
     ];
+
+    /**
+     * Set the 'type' field of the Post model based on the original URL
+     *
+     * @return void
+     */
+    public function setMediaType(): void
+    {
+        $this->type = $this->selectMediaType($this->original_url);
+    }
+
+    public function setOriginalUrl(string $url): void
+    {
+        switch ($this->selectMediaType($url)) {
+            case 'youtube':
+                $this->original_url = $this->getYoutubeUrl($url);
+                break;
+            case 'tiktok':
+                throw new NotImplementedException();
+        }
+    }
+
+    public static function selectMediaType(string $url): string|null
+    {
+        $platforms = config('supported_platforms.platforms', []);
+
+        foreach ($platforms as $key => $platform) {
+            foreach ($platform['urls'] as $baseUrl) {
+                if (str_starts_with($url, $baseUrl)) {
+                    return $key;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static function getYoutubeUrl(string $url): string|null
+    {
+        $parsedUrl = parse_url($url);
+        $urlQueries = $parsedUrl['query'];
+        $queryParameters = explode('&', $urlQueries);
+
+        foreach ($queryParameters as $param) {
+            $key = explode('=', $param)[0];
+
+            if ($key == 'v') {
+                return $parsedUrl['scheme'] . '://'
+                    . $parsedUrl['host'] . $parsedUrl['path'] . '?'
+                    . $param;
+            }
+        }
+
+        return null;
+    }
 }
