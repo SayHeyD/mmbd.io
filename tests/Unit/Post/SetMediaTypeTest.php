21f683401f27265ae568c5247f6b801c2170cb7d
David Docampo <david@opsone.ch>
2024-02-11 01:07:37 +0100

    Add tests for SetMediaType and SetOriginalUrl functionality
    Here, new unit tests have been added to validate the SetMediaType and SetOriginalUrl functionality in the Post class. These tests assert that the correct media type is set based on the post's URL and that the original URL is set correctly with accurate parsing.

---
+++ tests/Unit/Post/SetMediaTypeTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Unit\Post;
+
+use App\Models\Post;
+use Tests\TestCase;
+
+class SetMediaTypeTest extends TestCase
+{
+    /**
+     * Tests if the model field 'type' gets set to YouTube
+     * (https://www.youtube.com/watch?v=0jYQ58e1yXs)
+     *
+     * @test
+     */
+    public function set_media_type_sets_model_type_field_to_youtube_with_normal_youtube_url(): void
+    {
+        $originalYouTubeUrl = 'https://www.youtube.com/watch?v=0jYQ58e1yXs';
+
+        $post = app(Post::class);
+
+        $post->original_url = $originalYouTubeUrl;
+
+        $post->setMediaType();
+
+        $this->assertEquals('youtube', $post->type);
+    }
+
+    /**
+     * Tests if the model field 'type' gets set to tiktok
+     * (https://www.youtube.com/watch?v=0jYQ58e1yXs)
+     *
+     * @test
+     */
+    public function set_media_type_sets_model_type_field_to_tiktok_with_normal_tiktok_url(): void
+    {
+        $originalTikTokUrl = 'https://tiktok.com/@hex1ab/video/7251661199321107739';
+
+        $post = app(Post::class);
+
+        $post->original_url = $originalTikTokUrl;
+
+        $post->setMediaType();
+
+        $this->assertEquals('tiktok', $post->type);
+    }
+
+    /**
+     * Tests if the model field 'type' gets set to null if base url is invalid
+     * (https://www.youtube.com/watch?v=0jYQ58e1yXs)
+     *
+     * @test
+     */
+    public function set_media_type_sets_model_type_field_to_null_with_invalid_base_url(): void
+    {
+        $originalYouTubeUrl = 'https://www.google.com/maps';
+
+        $post = app(Post::class);
+
+        $post->original_url = $originalYouTubeUrl;
+
+        $post->setMediaType();
+
+        $this->assertEquals(null, $post->type);
+    }
+}
