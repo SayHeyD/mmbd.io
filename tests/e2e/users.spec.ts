b14aeea082c3ef46d2d405e87e0fac1ba9a725be
David Docampo <david@docampo.ch>
2024-02-19 00:03:16 +0100

    use different emails for different browsers

--- tests/e2e/users.spec.ts
+++ tests/e2e/users.spec.ts
@@ -1,11 +1,29 @@
 import {expect, test} from '@playwright/test';
 import common from './common';
 
-test('Admin user can reset password', async ({ page }) => {
+const testEmails = {
+  chromium: 'david+chromium@docampo.ch',
+  firefox: 'david+firefox@docampo.ch',
+  webkit: 'david+webkit@docampo.ch',
+}
+
+test('Admin user can reset password', async ({ page, context }) => {
   await page.goto(`${common.appUrl}/login`);
   await page.getByRole('link', { name: 'Forgot your password?' }).click();
   await page.waitForURL('**/forgot-password');
-  await page.getByLabel('Email').fill('david@docampo.ch');
+
+  switch (context.browser().browserType().name()) {
+    case 'chromium':
+      await page.getByLabel('Email').fill(testEmails.chromium);
+      break
+    case 'firefox':
+      await page.getByLabel('Email').fill(testEmails.firefox);
+      break
+    case 'webkit':
+      await page.getByLabel('Email').fill(testEmails.webkit);
+      break
+  }
+
   await page.getByRole('button', { name: 'Email Password Reset Link' }).click();
   await page.waitForURL('**/forgot-password', {
     waitUntil: 'domcontentloaded',
