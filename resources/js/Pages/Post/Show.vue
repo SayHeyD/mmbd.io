cf2130ef916fe1eb7a766e0b1ebf04ce4783d3ce
David Docampo <david@docampo.ch>
2024-03-15 22:21:37 +0100

    fix view styling

--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -19,7 +19,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <PublicLayout title="Success">
+  <PublicLayout title="Success" class="overflow-x-hidden">
     <Head>
       <meta v-if="post.name"
             property="og:title" content="mmbd.io - {{ post.name }}" />
@@ -31,18 +31,9 @@ onMounted(() => {
       <meta property="og:description" content="View your favourite social media posts without any cookie banners!">
     </Head>
 
-    <div class="relative sm:flex sm:justify-center sm:items-center selection:bg-red-500 selection:text-white">
-      <div v-if="canLogin && false" class="sm:fixed sm:top-0 sm:end-0 p-6 text-end z-10">
-        <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
+    <div class="w-screen sm:flex sm:justify-center sm:items-center selection:bg-red-500 selection:text-white">
 
-        <template v-else>
-          <Link :href="route('login')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</Link>
-
-          <Link v-if="canRegister" :href="route('register')" class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</Link>
-        </template>
-      </div>
-
-      <div class="w-screen mx-auto pt-32 p-6 lg:p-8">
+      <div class="w-full mx-auto pt-32 p-6 lg:p-8">
         <div class="text-gray-800 dark:text-gray-200 text-center flex flex-col justify-center items-center">
 
           <YouTube class="mb-8" v-if="post.type == 'youtube'" :post="post" />
@@ -56,14 +47,3 @@ onMounted(() => {
     </div>
   </PublicLayout>
 </template>
-
-<style>
-.bg-dots-darker {
-    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
-}
-@media (prefers-color-scheme: dark) {
-    .dark\:bg-dots-lighter {
-        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
-    }
-}
-</style>
