eb684b6ff870023f9a82b2f1903e67abf36ec2e5
David Docampo <david@docampo.ch>
2024-03-15 15:20:38 +0100

    shift login button to right side of screen

--- resources/js/Layouts/PublicLayout.vue
+++ resources/js/Layouts/PublicLayout.vue
@@ -26,7 +26,7 @@ if (page.props.canRegister) {
     <Head :title="title" />
 
 
-    <div v-if="canLogin" class="sm:top-0 sm:end-0 text-end z-10">
+    <div v-if="canLogin" class="self-end p-4 z-10">
       <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
 
       <template v-else>
