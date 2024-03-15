631cfba150ff7e02a95cf9784f6c7a0594db463f
David Docampo <david@docampo.ch>
2024-03-15 23:39:26 +0100

    WIP: Platform detector

---
+++ resources/js/Components/Index/PlatformDetector.vue
@@ -0,0 +1,31 @@
+<script setup>
+//** Logos **//
+
+// YouTube
+import YouTubeDarkLogo from '../../../img/logos/youtube_dark.png'
+import YouTubeLightLogo from '../../../img/logos/youtube_light.png'
+
+// TikTok
+import TikTokDarkLogo from '../../../img/logos/tiktok_dark.png'
+import TikTokLightLogo from '../../../img/logos/tiktok_light.png'
+
+const props = defineProps({
+    url: String
+})
+
+</script>
+
+<template>
+  <div class="max-w-xl mx-auto flex flex-wrap">
+
+    <div class="flex items-center justify-around px-6 py-2 rounded text-red-500 border-red-500">
+      <img :src="YouTubeDarkLogo" class="hidden dark:block w-32" alt="YouTube Logo">
+      <img :src="YouTubeLightLogo" class="block dark:hidden w-32" alt="YouTube Logo">
+    </div>
+
+    <div class="flex items-center justify-around px-6 py-2 rounded text-red-500 border-red-500">
+      <img :src="TikTokDarkLogo" class="hidden dark:block w-32" alt="YouTube Logo">
+      <img :src="TikTokLightLogo" class="block dark:hidden w-32" alt="YouTube Logo">
+    </div>
+  </div>
+</template>
