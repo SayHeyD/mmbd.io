302832b91dcd76ed026a99bfa828ef0aed2c8891
David Docampo <david@opsone.ch>
2024-03-17 23:24:13 +0100

    remove selection styling

--- resources/js/Layouts/PublicLayout.vue
+++ resources/js/Layouts/PublicLayout.vue
@@ -22,7 +22,7 @@ if (page.props.canRegister) {
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center sm:flex sm:justify-center sm:items-center min-h-svh bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+  <div class="flex flex-col justify-center items-center sm:flex sm:justify-center sm:items-center min-h-svh bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900">
     <Head :title="title" />
 
 
