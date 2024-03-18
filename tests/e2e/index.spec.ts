e81a9ce9427c1d7428f99912d803fcfb1111cd2b
David Docampo <david@opsone.ch>
2024-03-18 01:02:26 +0100

    add test for how it works page

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -5,3 +5,13 @@ test('The index page loads', async ({ page, context }) => {
     await page.goto(`${common.appUrl}`);
     await expect(page.getByRole('heading', { name: 'mmbd.io' })).toBeVisible();
 });
+
+test('The how it works link is shown and redirects to correct page', async ({page}) => {
+    await page.goto(`${common.appUrl}`);
+    await expect(page.getByRole('link', { name: 'How it works' })).toBeVisible()
+    await page.getByRole('link', { name: 'How it works' }).click()
+    await page.waitForURL('**/how-it-works', {
+        waitUntil: 'domcontentloaded',
+    });
+    await expect(page.getByText('How it works')).toBeVisible()
+})
