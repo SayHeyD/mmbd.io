47c1ae98597d2db6d58678bf8241ed69a8cecbd4
David Docampo <david@opsone.ch>
2024-03-18 01:02:16 +0100

    remove unused code

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -4,8 +4,4 @@ import common from './common';
 test('The index page loads', async ({ page, context }) => {
     await page.goto(`${common.appUrl}`);
     await expect(page.getByRole('heading', { name: 'mmbd.io' })).toBeVisible();
-    // await page.getByText('Does your grandma get').click();
-    // await page.getByText('Paste the link to the post').click();
-    // await page.getByRole('button', { name: 'Get your link!' }).click();
-    // await page.getByRole('link', { name: 'How it works' }).click();
 });
