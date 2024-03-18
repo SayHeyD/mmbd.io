e4ae4c9e19e4a4837691dffe7a3b5c44a3dc9866
David Docampo <david@opsone.ch>
2024-03-18 09:49:14 +0100

    add safari and chrome on mobile to test devices

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,14 +60,20 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+        contextOptions: {
+          // chromium-specific permissions
+          permissions: ['clipboard-read'],
+        },
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
 
     /* Test against branded browsers. */
     // {
